3465a49faedc1bff6f5cf7f2ec2976ddb0e88689
Jwalanta Shrestha <jwalanta@gmail.com>
2019-12-01 07:54:10 -0800

    2019 - day 01

--- 2019/01/day01.py
+++ 2019/01/day01.py
@@ -5,27 +5,24 @@
 # Solution for https://adventofcode.com/2019/day/1
 #
 
-import math
+def parse_line(line):
+    return int(line)
 
-def calculateFuel(n):
+def read_input():
+    f = open("input.txt")
+    return [parse_line(l) for l in f.readlines()]
+
+def calculate_fuel(n):
     sum = 0
     while n > 0:
-        n = math.floor(n / 3) - 2
+        n = n//3-2
         if n < 0:
             break
         sum += n
 
-    return int(sum)
+    return sum
 
+input = read_input()
 
-f = open("input.txt")
-
-sum1 = 0
-sum2 = 0
-for line in f.readlines():
-    sum1 += int(math.floor(int(line) / 3) - 2)
-    sum2 += calculateFuel(int(line))
-
-
-print("Part 1: %d" % sum1)
-print("Part 2: %d" % sum2)
+print("Part 1: %d" % sum(map(lambda x: x//3-2, input)))
+print("Part 2: %d" % sum(map(calculate_fuel, input)))
